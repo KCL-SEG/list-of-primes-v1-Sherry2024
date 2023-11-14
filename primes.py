10e3cfa77699834af19f950178cd132626561539
Yiqing Ren <yiqing.1.ren@kcl.ac.uk>
2023-11-14 12:47:59 +0000

    List prime numbers

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range (2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
-    list = []
-    return list
+    prime_list = []
+    num = 2 
+    while len(prime_list) < number_of_primes:
+        if is_prime(num):
+            prime_list.append(num)
+        num += 1
+    return prime_list
+
